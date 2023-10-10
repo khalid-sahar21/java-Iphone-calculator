122f2bc6298b9d191c84237f4443e70d08b86a13
khalid sahar <khalidsahar987@gmail.com>
2023-10-10 00:45:48 -0700

    Java iphone calculator

---
+++ iphone_cal/src/main/java/khalid/iphone_cal/Iphone_cal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package khalid.iphone_cal;
+
+/**
+ *
+ * @author PRECiSiON
+ */
+public class Iphone_cal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
